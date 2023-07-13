59d58b62c8270b90a05d20440f53b8b60b91d9e7
Klymenko Illia <sovuh2703@gmail.com>
2023-07-13 20:30:23 +0300

    created category redirect and photo redirect, templates, styles

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -6,4 +6,6 @@ app_name = 'portfolio'
 
 urlpatterns = [
     path('', views.all_categories, name='portfolio'),
+    path('<int:pk>/', views.category_photos, name='category'),
+    path('photo/<int:pk>', views.photo, name='photo')
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from portfolio.models import Category
+from portfolio.models import Category, Photo
 
 
 def all_categories(request):
@@ -17,4 +17,36 @@ def all_categories(request):
         'column_1': category_columns['1'],
         'column_2': category_columns['2'],
         'column_3': category_columns['3']
+    })
+
+
+def category_photos(request, pk):
+    category = get_object_or_404(Category, pk=pk)
+    related_photos = Photo.objects.filter(category=category).order_by('id')
+    photo_columns = {'1': [], '2': []}
+    num = '1'
+    for photo in related_photos:
+        photo_columns[num].append(photo)
+        if num == '1':
+            num = '2'
+            continue
+        num = '1'
+    return render(request, 'portfolio/category.html', {
+        'column_1': photo_columns['1'],
+        'column_2': photo_columns['2'],
+        'category_name': category.name,
+    })
+
+
+def photo(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    category = photo.category
+    category_photos = Photo.objects.filter(category=photo.category).order_by('id')
+    previous_photo = category_photos.filter(id__lt=photo.id).last()
+    next_photo = category_photos.filter(id__gt=photo.id).first()
+    return render(request, 'portfolio/photo.html', {
+        'photo': photo,
+        'category': category,
+        'previous_photo': previous_photo,
+        'next_photo': next_photo,
     })
