9ad9bd803532dd388728ad7f74bf74591698fd9b
Klymenko Illia <sovuh2703@gmail.com>
2023-07-15 14:10:29 +0300

    created model, template and style for blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 
 def all_posts(request):
-    return render(request, 'blog/all.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/all.html', {
+        'posts': posts,
+    })
