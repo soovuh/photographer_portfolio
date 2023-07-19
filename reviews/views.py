497ecdaa66cec7df65cea26195dd0e1912ee4357
Klymenko Illia <sovuh2703@gmail.com>
2023-07-19 14:39:38 +0300

    created form, updated view and reviews template

--- reviews/views.py
+++ reviews/views.py
@@ -1,10 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
+from reviews.forms import ReviewForm
 from reviews.models import Review
 
 
 def all_reviews(request):
+    if request.method == 'POST':
+        form = ReviewForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('reviews:all_reviews')
+    else:
+        form = ReviewForm()
     reviews = Review.objects.all().order_by('-created_at')
     return render(request, 'reviews/all_reviews.html', {
         'reviews': reviews,
+        'form': form,
     })
+
